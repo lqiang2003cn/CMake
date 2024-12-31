d06607a21f9a45e125f221f7701dccc9fe1186fb
lqiang2003cn <randycoq@163.com>
2024-12-31 15:06:06 +0800

    i

--- Help/guide/tutorial/Step1/MathFunctions/MathFunctions.cxx
+++ Help/guide/tutorial/Step1/MathFunctions/MathFunctions.cxx
@@ -4,8 +4,10 @@
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
 #include <cstdio>
-
-#include "mysqrt.h"
+#ifdef LQTECH_USE_MYMATH
+#  include "mysqrt.h"
+#endif
+#include <cmath>
 
 namespace mathfunctions {
 double sqrt(double x)
@@ -13,6 +15,10 @@ double sqrt(double x)
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
   printf("good one");
+#ifdef LQTECH_USE_MYMATH
   return detail::mysqrt(x);
+#else
+  return std::sqrt(x);
+#endif
 }
 }
